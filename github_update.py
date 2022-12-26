8aa7eba0ddcc1eed9cad25747068527686d58210
nymath <nymath@163.com>
2022-12-26 21:58:15 +0800

    update files\, 2022-12-26 21:58:15

---
+++ github_update.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
+import datetime
+
+current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+try:
+    os.system(f'git add .')
+    os.system(f"git commit -m \"update files\, {current_time}\"")
+    os.system('!git push -u origin main')
+    print('Success')
+except:
+    print("ERROR")
