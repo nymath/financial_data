928e9bd675e209ab2e60b0a36be3fe7eed593d63
nymath <nymath@163.com>
2023-01-02 23:12:03 +0800

    update files, 2023-01-02 23:12:02

--- update_index_components.py
+++ update_index_components.py
@@ -4,20 +4,30 @@ import pickle
 import tushare as ts
 import time
 import sys
+import pandas as pd
 import datetime
+import requests
+import json
+
 rqdatac.init()
 current_time = datetime.datetime.now().strftime("%Y%m%d")
+path_current = os.getcwd()
+if not os.path.exists(path_current + '/data'):
+    os.mkdir(path_current + '/data')
+if not os.path.exists('./data/index_components'):
+    os.mkdir('./data/index_components')
 
 
 if __name__ == '__main__':
-    if not os.path.exists('./data/index_components'):
-        os.mkdir('./data/index_components')
         
     namespace = ['000300', '000016', '000905', '000906']
     for x in namespace:
         test = rqdatac.index_components(f'{x}.XSHG', start_date = '20050101', end_date=current_time)
-        with open(f'./data/index_components/{x}.XSHG.pkl', 'wb') as f:
-            pickle.dump(test,f)
+        keys = pd.to_datetime(list(test.keys()))
+        date = pd.to_datetime(list(test.keys())).strftime('%Y%m%d').to_list()
+        res = dict(zip(date,test.values()))
+        with open(f'./data/index_components/{x}.XSHG.json', 'w') as f:
+            f.write(json.dumps(res))
     print(f'successfully processed! The updated file is saved to {path_current}/data/index')      
 
 # with open('./data/index_components/test.pkl', 'rb') as f:
